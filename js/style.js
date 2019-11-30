b8fcd7699e07038bd86d29b53c3cf395c3381229
Sunny <47844914+Aftabe@users.noreply.github.com>
2019-11-30 11:36:16 +1100

    style added

--- js/style.js
+++ js/style.js
@@ -6,7 +6,18 @@ for (var i = 0; i < 10; i = i + 1) {
 
 var names = ["ALi", "Khaliq", "Hassan", "Sadiq"];
 for (var i = 0; i < names.length; i = i + 1) {
-    var name = names[i];
-    
-    alert(name);
-}
+  var name = names[i];
+
+  alert(name);
+};
+
+// js event handler
+
+(function () {
+  var el = document.getElementById('div-class');
+
+  el.onclick = function () {
+    this.style.backgroundColor = 'green';
+  };
+
+}());
