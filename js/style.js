5a5d02628e71c09684c5fcf987e67e3af10a539f
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-07-24 20:15:58 +1000

    new js statements added

--- js/style.js
+++ js/style.js
@@ -1,1 +1,11 @@
-const btn = document.querySelectorAll('button');
+(function () {
+    var buttons = document.getElementsByTagName("button");
+
+    for (var i = 0, len = buttons.length; i < len; i = i + 1) {
+        buttons[i].onclick = function () {
+            var className = this.innerHTML.toLowerCase();
+
+            document.body.className = className;
+        };
+    }
+}()); 
