0bfc88730235a05097d163282aa45e75085e1e4d
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-07-24 20:45:04 +1000

    js code block added

--- js/style.js
+++ js/style.js
@@ -9,3 +9,16 @@
         };
     }
 }()); 
+(function () {
+    var btns = document.getElementsByTagName("button");
+
+    for (var i = 0, len = btns.length; i < len; i = i + 1) {
+        btns[i].onclick = function () {
+            // var className = this.innerHTML.toLowerCase();
+            var inneCont = this.innerHTML.toUpperCase();
+
+            document.body.className = inneCont;
+        };
+    }
+}()); 
+
