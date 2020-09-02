ee028d80a32b751bdd665f5c58cf5c72170acacc
Aftabe <shamsasghari77@gmail.com>
2020-09-02 19:59:26 +1000

    testing

--- js/style.js
+++ js/style.js
@@ -8,16 +8,4 @@
             document.body.className = className;
         };
     }
-}()); 
-(function () {
-    var btns = document.getElementsByTagName("button");
-
-    for (var i = 0, len = btns.length; i < len; i = i + 1) {
-        btns[i].onclick = function () {
-            // var className = this.innerHTML.toLowerCase();
-            var inneCont = this.innerHTML.toUpperCase();
-
-            document.body.className = inneCont;
-        };
-    }
-}());           
+}());          
