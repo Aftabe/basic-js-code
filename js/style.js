45b0b9b022e9aadaf745a4ff99c376ad8d1f6f77
Aftabe <shamsasghari77@gmail.com>
2020-09-02 19:43:02 +1000

    code modified

--- js/style.js
+++ js/style.js
@@ -3,7 +3,7 @@
 
     for (var i = 0, len = buttons.length; i < len; i = i + 1) {
         buttons[i].onclick = function () {
-            var className = this.innerHTML.toLowerCase();
+            var className = this.innerHTML.toUpperCase();
 
             document.body.className = className;
         };
