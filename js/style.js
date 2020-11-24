1815bf6655e510a16ba2d0ea82cf7868059ca8e7
Aftabe <shamsasghari77@gmail.com>
2020-11-24 20:43:09 +1100

    some js code just modified

--- js/style.js
+++ js/style.js
@@ -4,21 +4,9 @@
     for (var i = 0, len = buttons.length; i < len; i = i + 1) {
         buttons[i].onclick = function () {
             var className = this.innerHTML.toUpperCase();
-            var newClass = this.style.color = 'green';
 
             document.body.className = className;
-            document.body.className = newClass;
 
         };
     }
 }());    
-
-(function(){
-    function changeSomeThing(pra){
-        var text;
-        if(pra == 1){
-            
-            
-        }
-    }
-}());
