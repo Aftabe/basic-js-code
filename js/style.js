350ed154b24ef5e48d1b0deef6b207de9d6b2171
Aftabe <shamsasghari77@gmail.com>
2020-11-24 20:33:39 +1100

    For testing

--- js/style.js
+++ js/style.js
@@ -4,8 +4,11 @@
     for (var i = 0, len = buttons.length; i < len; i = i + 1) {
         buttons[i].onclick = function () {
             var className = this.innerHTML.toUpperCase();
+            var newClass = this.style.color = 'green';
 
             document.body.className = className;
+            document.body.className = newClass;
+
         };
     }
 }());    
